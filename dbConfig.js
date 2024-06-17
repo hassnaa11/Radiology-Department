7d611eedcf937669a62f77865fe9e7ad756091d0
nariman-ahmed <nariman.sayed2003@gmail.com>
2024-06-18 02:05:07 +0300

    ..

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const { Pool } = require("pg");
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://hospitaldb_owner:lQLSC73Hwzhu@ep-falling-butterfly-a5jd6u66-pooler.us-east-2.aws.neon.tech/hospitaldb?sslmode=require`;
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
